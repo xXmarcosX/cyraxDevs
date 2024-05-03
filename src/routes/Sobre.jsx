d8a467f985ac92951f33b943a9ed5c03bb24f4eb
xXmarcosX <marcos.v3567@gmail.com>
2024-05-02 21:00:32 -0300

    adicionado html da página sobre

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,7 +1,21 @@
 function Sobre(){
     return (
         <>
-        <h1>Sobre</h1>
+        <div className="background">
+            <div className="background__container">
+                <div className="background__container__img">
+                    <img src="" alt="" />
+                </div>
+                <div className="background__container__content">
+                    <h1>sobre nós</h1>
+                    <div className="background__container__content__line"></div>
+                    <div className="background__container__descricao">
+                        <p>descricao</p>
+                        <p>Especialistas em Desenvolvimento de Software. Soluções inovadoras para o seu negócio</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
