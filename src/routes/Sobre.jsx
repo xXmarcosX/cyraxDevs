27c6ffc69b5c3e2f279d190f2fd26ed91e3e73c4
xXmarcosX <marcos.v3567@gmail.com>
2024-05-02 21:27:07 -0300

    finalização da página sobre

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,3 +1,6 @@
+import Balls from "../assets/sobre/balls.png"
+import Office from '../assets/sobre/office.png'
+
 import '../css/sobre.css'
 
 function Sobre(){
@@ -6,17 +9,18 @@ function Sobre(){
         <div className="background">
             <div className="background__container">
                 <div className="background__container__img">
-                    <img src="" alt="" />
+                    <img src={Balls} alt="" />
                 </div>
                 <div className="background__container__content">
                     <h1>sobre nós</h1>
                     <div className="background__container__content__line"></div>
                     <div className="background__container__descricao">
-                        <p>descricao</p>
-                        <p>Especialistas em Desenvolvimento de Software. Soluções inovadoras para o seu negócio</p>
+                        <p><strong>descricao</strong></p>
+                        <p className='background__container__descricao__p2'>Especialistas em Desenvolvimento de Software. Soluções inovadoras para o seu negócio</p>
                     </div>
                 </div>
             </div>
+            <img src={Office} alt="" className="background__img" />
         </div>
         </>
     )
